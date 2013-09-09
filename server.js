2076e574db08e1342c8ff79c874b95bff36aacde
Dion Almaer <dion@almaer.com>
2013-09-09 16:45:23 -0700

    the hapi node server

---
+++ server.js
@@ -0,0 +1,68 @@
+var Hapi = require('hapi');
+
+var port = process.env.PORT || 5000;
+
+var packageJSON = require("./package.json");
+
+var appVersion = packageJSON.version || '13.3.7';
+
+//
+// Start the server configuration engines!
+var server = new Hapi.Server(port, {
+	views: {
+		path: 'templates',
+		engines: {
+			html: {
+        module: handlebarsEngine()
+      }
+		}
+	}
+});
+
+server.route([{
+  method: 'GET',
+  path: '/',
+  handler: function(request) {
+    request.reply.view('index.html', { version: appVersion });
+  }
+}, {
+  method: 'GET',
+  path: '/{ignore}/{path*}',
+  config: {
+    cache: {
+      mode: 'client',
+      expiresIn: 31536000 /* 1000ms * 60s * 60min * 24hrs * 365days == 1 year! */
+    },
+    handler: {
+      directory: {
+        path: '.',
+        listing: false,
+        index: true
+      }
+    }
+  }
+}]);
+
+server.on('request', function (request, event, tags) {
+    if (tags.error) {
+        console.error(event);
+    }
+});
+
+server.start(function() {
+  console.log("The server is listening at", server.info.uri);
+});
+
+//
+
+function handlebarsEngine() {
+  var handlebars = require('handlebars');
+
+  var uniqueVersion = new Buffer(appVersion).toString('base64');
+
+  handlebars.registerHelper('uurl', function(url) {
+    return "/v" + uniqueVersion + "/" + url;
+  });
+
+  return handlebars;
+}
